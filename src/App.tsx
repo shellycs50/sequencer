15ab3f70a8d79e7914883559f101d64778a82ecb
Rob Sheldrick <lebaguettemuncherer@gmail.com>
2024-04-29 13:00:24 +0100

    added bpm slider, fixed audioContext mistake, added timing visualiser

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useRef, useEffect } from 'react'
 import { FaPlay, FaStop } from "react-icons/fa";
-import { Progress } from "@/components/ui/progress"
+// import { Progress } from "@/components/ui/progress"
 import './App.css'
 import Sequencer from './components/Sequencer/Sequencer';
 import hiHatFile from './assets/hihat.wav'
@@ -12,23 +12,25 @@ import TimeUI from './components/timeUI/TimeUI';
 
 
 function App() {
-  const audioContext = new AudioContext();
-  // const timeRef = useRef(0)
-  const [currentTime, _] = useState(0)
+  
+  const [audioContext, _] = useState(new AudioContext())
   const hiHatSound = loadAudioFile(hiHatFile)
   const kickSound = loadAudioFile(kickFile)
   const snareSound = loadAudioFile(snareFile)
   const openHiHatSound = loadAudioFile(openHiHatFile)
-  const [tempo, setTempo] = useState(140) 
+  const [tempo, setTempo] = useState(140)
   const secondsPerBeat = 60 / tempo
   const secondsPerQuarter = secondsPerBeat / 4
-  const secondsPerBar = secondsPerBeat * 4
+  // const secondsPerBar = secondsPerBeat * 4
   const [sequencerCollection, setSequencerCollection] = useState<Array<Array<boolean>>>(Array(4).fill(0).map(() => Array(16).fill(false)));
-  const sequencerPlayState = useRef(0) //which quaver we are on
+  const [sequencerPlayState, setSequencerPlayState] = useState(0)
   const [volumes, setVolumes] = useState<Array<number>>(new Array(4).fill(50))
   const [isPlaying, setIsPlaying] = useState(false)
   const playingSources = useRef<Array<AudioBufferSourceNode>>([])
   const tempoChanger = useRef<NodeJS.Timeout | null>(null);
+  const timeUISetter = useRef<NodeJS.Timeout | null>(null);
+  const playHandler = useRef<NodeJS.Timeout | null>(null);
+
   useEffect(() => {
     if (tempoChanger.current) {
       clearTimeout(tempoChanger.current)
@@ -36,7 +38,7 @@ function App() {
     tempoChanger.current = setTimeout(async () => {
       if (!isPlaying) return
       stopPlaying()
-      await delay(100)
+      
       startPlayingLoop()
     }, 500)
   }, [tempo])
@@ -44,36 +46,19 @@ function App() {
   function delay(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
-  useEffect(() => {
-    console.log(sequencerPlayState.current)
-  }, [sequencerPlayState.current])
 
-  // function giveTimeToState() {
-  //   timePasserRef.current = setInterval(() => {
-  //     if (audioContext.currentTime) {
-  //       setCurrentTime(audioContext.currentTime);
-  //     }
-  //   }, 50);
-  // }
-  // function stopGivingTimeToState() {
-  //   if (timePasserRef.current) {
-  //     clearTimeout(timePasserRef.current)
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   setInterval(() => {
-  //     let currentQuaver = sequencerPlayState.current
-  //     console.log({currentQuaver})
-  //   }, 100)
-  // })
 
   useEffect(() => {
+    restartWithDelay()
+  }, [sequencerCollection])
+
+  async function restartWithDelay() {
     if (!isPlaying) return;
 
     stopPlaying()
+    
     startPlayingLoop();
-  }, [sequencerCollection])
+  }
 
   async function loadAudioFile(url: string): Promise<AudioBuffer> {
     const response = await fetch(url);
@@ -87,23 +72,30 @@ function App() {
     source.connect(audioContext.destination);
     source.start(time);
     playingSources.current.push(source)
-    if (audioContext.currentTime == 100 * secondsPerBar) {
-      stopPlaying()
-      startPlayingLoop()
-    }
+    
   }
 
   async function startPlayingLoop() {
-    await delay(200)
-    // let currentQuaver = sequencerPlayState.current;
     let currentQuaver = 0
     const startTime = audioContext.currentTime
     const kickPattern = sequencerCollection[0]
     const snarePattern = sequencerCollection[1]
     const closedHatPattern = sequencerCollection[2]
     const openHatPattern = sequencerCollection[3]
+    
+    // setInterval to increment timeUI
+    if (timeUISetter.current) {
+      clearTimeout(timeUISetter.current)
+    }
+    timeUISetter.current = setInterval(() => {
 
-    for (let j = 0; j < 10; j++) {
+      setSequencerPlayState((prev) => (prev + 1) % 16)
+
+    }, secondsPerQuarter * 1000)
+    setSequencerPlayState(0)
+
+    //Main logic
+    for (let j = 0; j < 20; j++) {
       const barStartTime = startTime + (j * 4 * secondsPerBeat)
       for (let i = j == 0 ? currentQuaver : 0; i < sequencerCollection[0].length; i++) {
         const time = barStartTime + (i * secondsPerQuarter);
@@ -121,18 +113,30 @@ function App() {
         }
       }
     }
-
+    
   }
 
   function stopPlaying() {
-    // timeRef.current = audioContext.currentTime
-    playingSources.current.forEach(source => source.stop())
+    if (timeUISetter.current) {
+      clearTimeout(timeUISetter.current)
+    }
+    playingSources.current.forEach((source) => {
+      source.stop();
+      source.disconnect();
+    });
   }
 
   function togglePlay() {
-    // isPlaying ? stopGivingTimeToState() : giveTimeToState()
-    isPlaying ? stopPlaying() : startPlayingLoop()
-    setIsPlaying(!isPlaying)
+    if (isPlaying) {
+      stopPlaying()
+    }
+    else {
+      if (playHandler.current) clearTimeout(playHandler.current)
+      playHandler.current = setTimeout(() => {
+        startPlayingLoop()
+      }, 100)
+    }
+    setIsPlaying(!isPlaying);
   }
 
   function resetArray() {
@@ -145,23 +149,23 @@ function App() {
       <h1 className='text-4xl font-semibold my-10'>DRUM SEQUENCER</h1>
       <div className='w-3/4 flex flex-row justify-center gap-3'>
         <div className='flex flex-col gap-1'>
-        <p>BPM</p>
-        <p>{tempo}</p>
+          <p>BPM</p>
+          <p>{tempo}</p>
         </div>
-        
-        <Slider defaultValue={[tempo]} max={500} step={1} onValueChange={(e: Array<any>) => parseInt(e[0]) && setTempo(parseInt(e[0]))} />
+
+        <Slider className='w-1/2' defaultValue={[tempo]} max={500} step={1} onValueChange={(e: Array<any>) => parseInt(e[0]) && setTempo(parseInt(e[0]))} />
       </div>
       <Sequencer sequencerCollection={sequencerCollection} setSequencerCollection={setSequencerCollection} volumes={volumes} setVolumes={setVolumes} />
       <div className='w-7/12 border-t-2 border-black'></div>
-      <TimeUI sequencerPlayState={sequencerPlayState}/>
+      <TimeUI sequencerPlayState={sequencerPlayState} />
       <div className='flex flex-row gap-3'>
         <a onClick={togglePlay} className={`text-4xl cursor-pointer p-5 rounded-full ${isPlaying ? "bg-red-300" : "bg-green-300"}`}>{isPlaying ? <FaStop /> : <FaPlay />}</a>
         <a onClick={resetArray} className='font-semibold text-2xl cursor-pointer p-5 rounded-full bg-slate-300'>Reset</a>
       </div>
-    \
-      <Progress value={currentTime % secondsPerBar * 100} className='w-3/4' />
+      \
+      
       <div className='w-full border-black'>
-        
+
       </div>
 
     </div>
--- src/components/timeUI/TimeUI.tsx
+++ src/components/timeUI/TimeUI.tsx
@@ -1,31 +1,31 @@
 import TimeQuaver from "./TimeQuaver";
 
-function TimeUI({ sequencerPlayState }: { sequencerPlayState: React.MutableRefObject<number> }) {
+function TimeUI({ sequencerPlayState }: { sequencerPlayState: number }) {
     return (
         <div className='flex flex-row gap-8'>
             <div className='flex flex-row gap-1'>
-                <TimeQuaver shouldLight={sequencerPlayState.current == 1} />
-                <TimeQuaver shouldLight={sequencerPlayState.current == 2} />
-                <TimeQuaver shouldLight={sequencerPlayState.current == 3} />
-                <TimeQuaver shouldLight={sequencerPlayState.current == 4} />
+                <TimeQuaver shouldLight={sequencerPlayState == 1} />
+                <TimeQuaver shouldLight={sequencerPlayState == 2} />
+                <TimeQuaver shouldLight={sequencerPlayState == 3} />
+                <TimeQuaver shouldLight={sequencerPlayState == 4} />
             </div>
             <div className='flex flex-row gap-1'>
-                <TimeQuaver shouldLight={sequencerPlayState.current == 5} />
-                <TimeQuaver shouldLight={sequencerPlayState.current == 6} />
-                <TimeQuaver shouldLight={sequencerPlayState.current == 7} />
-                <TimeQuaver shouldLight={sequencerPlayState.current == 8} />
+                <TimeQuaver shouldLight={sequencerPlayState == 5} />
+                <TimeQuaver shouldLight={sequencerPlayState == 6} />
+                <TimeQuaver shouldLight={sequencerPlayState == 7} />
+                <TimeQuaver shouldLight={sequencerPlayState == 8} />
             </div>
             <div className='flex flex-row gap-1'>
-                <TimeQuaver shouldLight={sequencerPlayState.current == 9} />
-                <TimeQuaver shouldLight={sequencerPlayState.current == 10} />
-                <TimeQuaver shouldLight={sequencerPlayState.current == 11} />
-                <TimeQuaver shouldLight={sequencerPlayState.current == 12} />
+                <TimeQuaver shouldLight={sequencerPlayState == 9} />
+                <TimeQuaver shouldLight={sequencerPlayState == 10} />
+                <TimeQuaver shouldLight={sequencerPlayState == 11} />
+                <TimeQuaver shouldLight={sequencerPlayState == 12} />
             </div>
             <div className='flex flex-row gap-1'>
-                <TimeQuaver shouldLight={sequencerPlayState.current == 13} />
-                <TimeQuaver shouldLight={sequencerPlayState.current == 14} />
-                <TimeQuaver shouldLight={sequencerPlayState.current == 15} />
-                <TimeQuaver shouldLight={sequencerPlayState.current == 16} />
+                <TimeQuaver shouldLight={sequencerPlayState == 13} />
+                <TimeQuaver shouldLight={sequencerPlayState == 14} />
+                <TimeQuaver shouldLight={sequencerPlayState == 15} />
+                <TimeQuaver shouldLight={sequencerPlayState == 16} />
             </div>
         </div>
     );
